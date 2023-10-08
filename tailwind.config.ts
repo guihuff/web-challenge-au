0701b800b37f61ca23e0f9d0752f40c3b7ac6afd
Guilherme Feier Huff <ghuffg00@gmail.com>
2023-10-08 11:36:22 -0300

    first configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'color-primary': '#064C4F',
+        'color-secundary': '#F7F2DD',
+        'background-primary': '#F3F6F5',
+        'background-secundary': '#064C4F',
+        'button-primary': '#BBEA70',
+      }
     },
   },
   plugins: [],
