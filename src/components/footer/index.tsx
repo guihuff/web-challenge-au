eed79ddc098388971ee5d8371ede95850fefb62d
Guilherme Feier Huff <ghuffg00@gmail.com>
2023-10-08 17:29:00 -0300

    footer responsive

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
   return (
     <footer className="container m-auto p-3"  >
-      <div className="w-full flex justify-between px-3 py-1 bg-white rounded-xl">
-        <p>Autor: Guilherme Huff</p>
+      <div className="w-full flex flex-col sm:flex-row justify-between px-3 py-1 bg-white rounded-xl text-center sm:text-start">
+        <p className="leading-10 sm:leading-normal">Autor: Guilherme Huff</p>
         <a href="https://storyset.com/people">Illustrations by Storyset</a>
       </div>
     </footer>
