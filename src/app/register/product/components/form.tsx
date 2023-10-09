d741bb33be4e2efcdffc289fc85d65548348f112
Guilherme Feier Huff <ghuffg00@gmail.com>
2023-10-09 14:03:41 -0300

    New product form

--- src/app/register/product/components/form.tsx
+++ src/app/register/product/components/form.tsx
@@ -1,25 +1,52 @@
 "use client"
 
-import { Input } from "@/components/defaultInput"
+import { Input, Select } from "@/components/defaultInput"
 import { Loader } from "@/components/loader";
+import { CategoryProps } from "@/interfaces/category";
 import { api } from "@/services/api";
 import { FormEvent, useState } from "react";
 import { toast } from "react-toastify";
 
-export function Form() {
+interface FormProps {
+  categories: CategoryProps[];
+}
+
+export function Form({ categories }: FormProps) {
+  
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [priceInput, setPriceInput] = useState<string>("");
+  const [price, setPrice] = useState<number>(0);
+  const [categoryId, setCategoryId] = useState<string>('');
+
+
   const [loading, setLoading] = useState(false);
 
+
+  const handleInputPriceInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let sanitizedValue = e.target.value.replace(/[^0-9,]/g, '');
+
+    const dotCount = sanitizedValue.split(',').length - 1;
+    if (dotCount > 1) {
+      sanitizedValue = sanitizedValue.slice(0, sanitizedValue.lastIndexOf(','));
+    }
+
+    const parts = sanitizedValue.split(',');
+    if (parts[1] && parts[1].length > 2) {
+      parts[1] = parts[1].slice(0, 2);
+    }
+    setPrice(parseFloat(parts.join('.')));
+    setPriceInput(parts.join(','));
+  }
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     setLoading(true);
     if(!validate ()) {
       try {
-        const response = await api.post("/categories", { name, description });
+        const response = await api.post("/products", { name, description, price, id_category: categoryId })
         if(response.status === 201){
-          toast.success('Categoria cadastrada com sucesso!', {
+          toast.success('Produto cadastrada com sucesso!', {
             position: "top-center",
             autoClose: 5000,
             hideProgressBar: false,
@@ -64,11 +91,19 @@ export function Form() {
   };
 
   const validateDescription = () => {
-    return description.length > 4 ? true : false
+    return description.length > 4 ? true : false;
   };
 
+  const validatePrice = () => {
+    return price > 0 ? true : false;
+  }
+
+  const validateCategory = () => {
+    return categoryId.length > 0 ? true : false;
+  }
+
   const validate = () => {
-    const result: boolean[] = [validadeName(), validateDescription()];
+    const result: boolean[] = [validadeName(), validateDescription(), validatePrice(), validateCategory()];
     return result.indexOf(false) === -1 ? false : true;
   };
 
@@ -77,19 +112,34 @@ export function Form() {
       <div className="flex flex-col gap-3 px-3 py-6 shadow-lg rounded-xl">
         <h2 className="text-xl">Formulário</h2>
         <Input name={"Nome"} 
-          placeholder="nome da categoria" 
+          placeholder="nome do produto" 
           type="text" 
           value={name}
           onChange={(e) => setName(e.target.value)}
           isRequired
         />
         <Input name={"Descrição"} 
-          placeholder="descrição da categoria" 
+          placeholder="descrição do produto" 
           type="text" 
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           isRequired
         />
+        <Input name={"Preço"} 
+          placeholder="preço do produto" 
+          type="text"
+          value={priceInput}
+          onChange={handleInputPriceInput}
+          isRequired
+        />
+        <Select 
+          value={categoryId}
+          onChange={e => setCategoryId(e.target.value)}
+          options={categories}
+          name={"Categoria"}
+          required
+        />
+
         <button type="submit" 
           className="flex justify-center items-center h-12 mt-3 py-3 px-5 rounded-xl font-bold bg-button-primary hover:text-color-secundary hover:shadow-lg transition ease-in-out">
           {loading ? <Loader />: "Cadastrar"}
--- src/components/defaultInput/index.tsx
+++ src/components/defaultInput/index.tsx
@@ -1,4 +1,5 @@
-import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
+import { CategoryProps } from '@/interfaces/category';
+import { InputHTMLAttributes, ReactNode, SelectHTMLAttributes, TextareaHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name?: string;
@@ -9,6 +10,27 @@ export function Input({ name, isRequired, ...rest }: InputProps) {
   return (
     <label className='flex flex-col text-sm my-1'>{isRequired ? `${name} *`: name} 
       <input {...rest} className='h-10 rounded-xl px-3 placeholder:text-zinc-600 focus:outline-none focus-within:shadow-lg' />
+    </label>
+  )
+}
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name?: string;
+  isRequired?: boolean;
+  options: CategoryProps[];
+}
+export function Select({ name, isRequired, options, ...rest }: SelectProps) {
+  return (
+    <label className='flex flex-col text-sm my-1'>{isRequired ? `${name} *`: name} 
+      <div className='h-10 rounded-xl px-3 w-full bg-white pr-4'>
+        <select {...rest} className='h-full w-full bg-white focus:outline-none' >
+          <option value={""}>Selecione uma categoria</option>
+          {options.map(item => {
+            return <option key={item.id} value={item.id}>{item.name}</option>
+          })}
+        </select>
+      </div>
+      
     </label>
   )
 }
